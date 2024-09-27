50970c0540e999e560c804fbb6a6ffd2715f2d82
DenzelNavarro <denzerur@gmail.com>
2024-09-28 01:43:26 +0800

    commit js

---
+++ script.js
@@ -0,0 +1,82 @@
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.getElementById("options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+
+let playerScore = 0;
+let computerScore = 0;
+
+function getRandomComputerResult() {
+    const options = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex]
+}
+
+function hasPlayerWonTheRound(player, computer) {
+    return (
+        (player === "Rock" && computer === "Scissors") ||
+        (player === "Scissors" && computer === "Paper") ||
+        (player === "Paper" && computer === "Rock")
+    );
+}
+
+function getRoundResults(userOption) {
+    const computerResult = getRandomComputerResult();
+
+    if(hasPlayerWonTheRound(userOption, computerResult)) {
+        playerScore++;
+        return `Player wins! ${userOption} beats ${computerResult}`;
+    } else if(computerScore === userOption) {
+        return `It's a tie! Both chose ${userOption}`;
+    } else {
+        computerScore++;
+        return `Computer wins! ${computerResult} beats ${userOption}`;
+    }
+}
+
+function showResults(userOption) {
+    roundResultsMsg.innerText = getRoundResults(userOption);
+    computerScoreSpanElement.innerText = computerScore;
+    playerScoreSpanElement.innerText = playerScore;
+
+    if(playerScore === 3 || computerScore === 3) {
+        if(playerScore === 3) {
+            winnerMsgElement.innerText = "Computer is a monkey!"
+        } else {
+            winnerMsgElement.innerText = "You are a monkey"
+        }
+
+        resetGameBtn.style.display = "block";
+        optionsContainer.style.display = "none";
+    }
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSpanElement.innerText = playerScore;
+    computerScoreSpanElement.innerText = computerScore;
+    resetGameBtn.style.display = "none";
+    optionsContainer.style.display = "block";
+    winnerMsgElement.innerText = "";
+    roundResultsMsg.innerText = "";
+}
+
+resetGameBtn.addEventListener("click", resetGame);
+
+rockBtn.addEventListener("click", function () {
+    showResults("Rock");
+  });
+  
+paperBtn.addEventListener("click", function () {
+    showResults("Paper");
+  });
+  
+scissorsBtn.addEventListener("click", function () {
+    showResults("Scissors");
+  });
